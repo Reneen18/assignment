9f3df1daddf3b4bed0e03e31e864db8af28493d7
Reneen18 <renukanaga1823@gmail.com>
2025-03-18 09:28:03 +0530

    Update sql1.sql

--- sql/assignment/sql1.sql
+++ sql/assignment/sql1.sql
@@ -1,1 +1,15 @@
+use marchdb
 
+
+
+
+alter table trainee add email varchar(50) null
+
+
+alter table trainee drop column email
+
+alter table employee add constraint pk_id primary key(id)
+
+create table employeesalary(
+salaryid int primary key not null
+)
