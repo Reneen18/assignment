7ce0bbca22edb9113197c4c763113bbe0ec94564
Reneen18 <renukanaga1823@gmail.com>
2025-03-18 09:29:16 +0530

    Update sql1.sql

--- sql/assignment/sql1.sql
+++ sql/assignment/sql1.sql
@@ -1,15 +1,13 @@
-use marchdb
-
-
-
-
-alter table trainee add email varchar(50) null
-
-
-alter table trainee drop column email
-
-alter table employee add constraint pk_id primary key(id)
-
-create table employeesalary(
-salaryid int primary key not null
-)
+create database testdb2
+ 
+ select name from master.sys.databases
+ 
+ select * from master.sys.master_files
+ 
+ exec sp_databases
+ 
+ select name as DATABASE_NAME,size as DATABASE_SIZE,physical_name as Location from master.sys.master_files
+ 
+ alter database testdb2 modify name=marchdb
+ 
+ drop database if exists testdb2
