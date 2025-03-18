74dd01b3adf702b630ede416b7ae6817fd190a94
Reneen18 <renukanaga1823@gmail.com>
2025-03-18 15:45:34 +0530

    Add files via upload

---
+++ sql/assignment/SQLQuery10.sql
@@ -0,0 +1,6 @@
+--sorting records by descending order
+select * from employee order by ID desc
+--sorting records by ascending order
+select * from employee order by ID asc
+--sort table by name attribute
+select * from employee order by name desc
